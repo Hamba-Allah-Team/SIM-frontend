8bfae6a2ff75232580dcd48700a4456f70de678a
Cfathurrohman <cfathurrohman@gmail.com>
2025-05-16 22:19:43 +0700

    Feat: Add reset password insert code UI

---
+++ src/app/reset-password/verify/page.tsx
@@ -0,0 +1,17 @@
+import ResetPasswordCode from "@/components/ResetPasswordCode";
+import MosqueImage from "@/components/MosqueImage";
+
+export default function ResetPasswordCodePage() {
+  return (
+    <main className="flex h-screen w-screen overflow-hidden">
+      <div className="flex w-full flex-col md:flex-row">
+        <div className="flex-1 flex items-center justify-center">
+          <ResetPasswordCode />
+        </div>
+        <div className="flex-1 flex items-center justify-center">
+          <MosqueImage />
+        </div>
+      </div>
+    </main>
+  );
+}
