33a7902310ba0b3e9496665412ee51caa0ac98f7
DeaPutri17 <putrinastitidea@gmail.com>
2025-05-17 16:09:16 +0700

    feat: button tambah

---
+++ src/components/ButtonTambahClient.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Grid2x2Plus } from 'lucide-react' 
+import React from 'react'
+
+export default function ButtonTambahClient() {
+  const handleTambah = () => {
+    alert('Tambah konten masjid')
+  }
+
+  return (
+    <button
+      onClick={handleTambah}
+      className="flex items-center gap-2 px-4 py-2 rounded-xl transition"
+      style={{
+        backgroundColor: 'rgba(255, 147, 87, 0.1)', 
+        color: 'var(--color-custom-orange)',
+      }}
+    >
+      <Grid2x2Plus size={18} />
+      Tambah
+    </button>
+  )
+}
