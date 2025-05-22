e028768ef20720c3923830ce0c88a77df667221b
Haidarly2 <haidar.aly007@gmail.com>
2025-05-22 07:35:40 +0700

    feat: create DashboardPage component for Super Admin with welcome message

---
+++ src/app/superadmin/dashboard/page.tsx
@@ -0,0 +1,9 @@
+// app/(superadmin)/dashboard/page.tsx
+
+export default function DashboardPage() {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Selamat Datang di Dashboard Super Admin</h1>
+        </div>
+    )
+}
